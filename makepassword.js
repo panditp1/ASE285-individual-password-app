d7958ca135e2c388de3d3f6598b836abad57bd32
Priyanka Pandit <117121337+panditp1@users.noreply.github.com>
2025-05-01 16:15:55 -0400

    Step 1 complete: Hashed passwords and created base files

---
+++ makepassword.js
@@ -0,0 +1,22 @@
+'use strict'
+const {readFile, writeFile, hash} = require('./utility')
+
+function makepassword(passwordFileName, passwordEncFileName) {
+    const lines = readFile(passwordFileName);
+    const encryptedLines = [];
+
+    for (let line of lines) {
+        if (line.trim() === '') continue;
+        const [email, password] = line.split(':');
+        const hashedPassword = hash(password);
+        encryptedLines.push(`${email}:${hashedPassword}`);
+    }
+
+    writeFile(encryptedLines, passwordEncFileName);
+}
+
+if (require.main === module) {
+    makepassword('./password.txt', './password.enc.txt')
+}
+
+module.exports = { makepassword };
